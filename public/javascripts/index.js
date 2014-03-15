92f34946dcbcdf40ae9df1b5da6c7378e69af29a
kitsune <hime@kitsunelab.com>
2014-03-14 20:27:00 -0400

    with remove

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -57,6 +57,7 @@ $(document).ready(function(){
             {
                 list.push({suit:selected[i].getAttribute('msuit'), value:selected[i].getAttribute('mvalue')})
                 $('#' + selected[i].getAttribute('id')).css('display', 'none');
+                $('#' + selected[i].getAttribute('id')).css('display', 'none');
             }
             console.log(list);
             send_msg ('usecard', list);
