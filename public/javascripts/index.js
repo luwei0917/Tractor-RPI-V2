83307faabbefd4b4b3d5248a38cedcee958975b7
andyjiang <ll3992@gmail.com>
2014-04-14 23:48:28 -0400

    11:48PM 4/14/2014

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,107 +1,102 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     //window.open('/room1','_self');
-    $('#gamestart').click(function(){
+    $('#gamestart').click(function () {
         $('#gamestart').fadeOut('fast');
         //$('#anewroom').load('/room1')
         //connect_to_server();
-        send_msg('PlayerName','#inputName');
-        window.open('/room1','_self');
+        //send_msg('PlayerName','#inputName');
+        window.open('/room1', '_self');
     });
 
-    $('#button1').click(function() {
-        send_msg ('button', 'button1');
+    $('#button1').click(function () {
+        send_msg('button', 'button1');
     });
 
-    $('#gamestart p').mouseenter(function() {
-        $('#gamestart p').css('color','yellow');
+    $('#gamestart p').mouseenter(function () {
+        $('#gamestart p').css('color', 'yellow');
         //$('this').fadeOut('slow')
-    }).mouseleave(function() {
-            $('#gamestart p').css('color','violet');
-            //$('this').fadeOut('slow')
-        });
+    }).mouseleave(function () {
+        $('#gamestart p').css('color', 'violet');
+        //$('this').fadeOut('slow')
+    });
 
-    $('#inputbutton').click(function() {
-        if(myturn){
+    $('#inputbutton').click(function () {
+        if (myturn) {
             var res = $('#input1').val().split(' ');
             var cd = {suit: res[0], value: res[1]};
-            send_msg ('usecard', cd);
+            send_msg('usecard', cd);
             $('#input1').val('')
         }
-        else{
+        else {
             $('#gogogo').text('Not your turn yet, Do not panic');
         }
     })
-    $('#input1').keypress(function(k) {
+    $('#input1').keypress(function (k) {
 
-        if (k.which === 13)
-        {
-            if (myturn){
+        if (k.which === 13) {
+            if (myturn) {
                 var res = $('#input1').val().split(' ');
                 var cd = {suit: res[0], value: res[1]};
-                send_msg ('usecard', cd);
+                send_msg('usecard', cd);
                 $('#input1').val('')
             }
-            else{
+            else {
                 $('#gogogo').text('Not your turn yet, Do not panic');
             }
         }
     })
 
 
-
-    $('#playbutton').click(function() {
-        if(myturn){
+    $('#playbutton').click(function () {
+        if (myturn) {
             var list = [];
             var selected = $('#overlay').find('.select');
-            for (var i = 0; i < selected.length; i++)
-            {
-                list.push({suit:selected[i].getAttribute('msuit'), value:parseInt(selected[i].getAttribute('mvalue') )} )
+            for (var i = 0; i < selected.length; i++) {
+                list.push({suit: selected[i].getAttribute('msuit'), value: parseInt(selected[i].getAttribute('mvalue'))})
                 $('#' + selected[i].getAttribute('id')).css('display', 'none');
                 $('#' + selected[i].getAttribute('id')).removeClass('select');
                 //hehe
             }
             console.log(list);
-            send_msg ('usecard', list);
+            send_msg('usecard', list);
         }
-        else{
+        else {
             $('#gogogo').text('Not your turn yet, Do not panic');
         }
     })
 
-    $('#maidibutton').click(function() {
+    $('#maidibutton').click(function () {
         var list = [];
         var selected = $('#overlay').find('.select');
-        if (selected.length != 8)
-        {
+        if (selected.length != 8) {
             return alert('you have to select 8 cards');
         }
-        for (var i = 0; i < selected.length; i++)
-        {
-            list.push({suit:selected[i].getAttribute('msuit'), value:parseInt(selected[i].getAttribute('mvalue') )} )
+        for (var i = 0; i < selected.length; i++) {
+            list.push({suit: selected[i].getAttribute('msuit'), value: parseInt(selected[i].getAttribute('mvalue'))})
             $('#' + selected[i].getAttribute('id')).css('display', 'none');
             $('#' + selected[i].getAttribute('id')).removeClass('select');
             //hehe
         }
         console.log(list);
-        send_msg ('kittyCome', list);
+        send_msg('kittyCome', list);
         $('#maidibutton').css('visibility', 'hidden');
     })
 
 
 //DominantSuit
-    $('#IDominantSuit').click(function() {
-        if(DominantSuitChance){
+    $('#IDominantSuit').click(function () {
+        if (DominantSuitChance) {
             var card;
-            for(var i =0 ; i<chosen.length ; i++){
-                if(chosen[i]){
+            for (var i = 0; i < chosen.length; i++) {
+                if (chosen[i]) {
                     card = mycards[i];
                 }
             }
-            send_msg ('DominantSuitIn', card);
+            send_msg('DominantSuitIn', card);
 
         }
-        else{
+        else {
             $('#gogogo').text('No No No (one waving his finger in front of you');
         }
     })
